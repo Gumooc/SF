7bfea30022380dffa59f681e366ea84f4597c4ac
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-03 15:14:36 +0800

    Create insertuser.sql

---
+++ code/sql/insertuser.sql
@@ -0,0 +1,4 @@
+/*insert into user values(0,"sf1","sf",0,0,0,0,"1653395382@qq.com",1,0);
+insert into user values(1,"sf","sf",0,0,0,0,"1653395382@qq.com",1,0);
+insert into user values(2,"sf3","sf",0,0,0,0,"1653395382@qq.com",0,0);*/
+select * from user
