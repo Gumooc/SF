ccff0591c4d99d07f61eb69a6b49afa952d2a850
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-03 15:24:40 +0800

    Update Comment.java

--- code/back/src/main/java/com/run/entity/Comment.java
+++ code/back/src/main/java/com/run/entity/Comment.java
@@ -6,7 +6,16 @@ public class Comment {
 	private int bid;
 	private int cid; 
 	private int uid;
-	private Date lst;
+	private String lst;
+	private String des;
+	
+	public void setDes(String des) {
+		this.des = des;
+	}
+	
+	public String getDes() {
+		return des;
+	}
 	
 	public void setBid(int bid) {
 		this.bid = bid;
@@ -32,11 +41,11 @@ public class Comment {
 		return uid;
 	}
 
-	public Date getLst() {
+	public String getLst() {
 		return this.lst;
 	}
 	
-	public void setLst(Date lst) {
+	public void setLst(String lst) {
 		this.lst = lst;
 	}
 }
