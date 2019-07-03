a92d45cd16a37b96cb29546744e7196d5b0dd2a7
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-03 09:04:56 +0800

    Update UserService.java

--- code/back/src/main/java/com/run/service/UserService.java
+++ code/back/src/main/java/com/run/service/UserService.java
@@ -1,12 +1,10 @@
 package com.run.service;
 
-import org.springframework.stereotype.Service;
-
 import com.run.entity.User;
 
 import net.sf.json.JSONObject;
 
 public interface UserService {
 	JSONObject logincheck(User user);
-	JSONObject useractive(User user);
+	JSONObject useractive(int uid);
 }
