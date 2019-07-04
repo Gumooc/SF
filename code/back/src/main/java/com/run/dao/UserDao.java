86d1f01368909721b36e5d17d66b17df867dd3eb
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-04 11:22:56 +0800

    Update UserDao.java

--- code/back/src/main/java/com/run/dao/UserDao.java
+++ code/back/src/main/java/com/run/dao/UserDao.java
@@ -12,11 +12,12 @@ import com.run.entity.User;
 @Repository
 public interface UserDao {
 	User logincheck(User user);
+	User registercheck(User user);
+	void register(User user);
 	void active(int uid);
 	Collector askcollector(int uid); 
 	Collector askhistory(int uid);
 	List<Comment> askcomment(int uid);
 	List<Book> askworks(int uid);
-	void register(User user);
 	int getmaxid();
 }
