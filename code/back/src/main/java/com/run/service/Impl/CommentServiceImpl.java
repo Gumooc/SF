6322d3e4eb3d4bb36fea1bef190bba0925707397
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-04 11:23:07 +0800

    Create CommentServiceImpl.java

---
+++ code/back/src/main/java/com/run/service/Impl/CommentServiceImpl.java
@@ -0,0 +1,62 @@
+package com.run.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import com.run.dao.CommentDao;
+import com.run.entity.Comment;
+import com.run.entity.CommentDes;
+import com.run.entity.DBindex;
+import com.run.service.CommentService;
+
+import net.sf.json.JSONObject;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+	@Autowired
+	private CommentDao commentMapper;
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	@Override
+	public JSONObject insert(Comment comment) {
+		JSONObject feedback = new JSONObject();
+		feedback.put("resp", "f");
+		CommentDes commentDes = new CommentDes();
+		Query query = new Query(Criteria.where("id").is("commentIndex"));
+		DBindex result=mongoTemplate.findOne(query, DBindex.class, "comment");
+		if (result == null ) {
+			result = new DBindex();
+			result.setId("commentIndex");
+			result.setValue(10);
+		}
+		System.out.println(result);
+		int index = result.getValue()+1;
+		comment.setCid(index);
+		commentDes.setId(comment.getCid());
+		commentDes.setDes(comment.getDes());
+		commentMapper.insert(comment);
+		mongoTemplate.save(commentDes,"comment");
+		result.setValue(index);
+		mongoTemplate.save(result,"comment");
+		feedback.put("resp", "s");
+		return feedback;
+	}
+
+	@Override
+	public JSONObject delete(Comment comment) {
+		JSONObject feedback = new JSONObject();
+		feedback.put("resp", "f");
+
+		Query query = new Query(Criteria.where("id").is(comment.getCid()));
+		mongoTemplate.remove(query, CommentDes.class);
+		commentMapper.delete(comment);
+
+		feedback.put("resp", "s");
+		return feedback;
+	}
+
+}
