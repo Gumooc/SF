222af6f7c90986520b9d5d286d0f22eb44a9d80d
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-05 16:53:52 +0800

    Update BookDao.java

--- code/back/src/main/java/com/run/dao/BookDao.java
+++ code/back/src/main/java/com/run/dao/BookDao.java
@@ -1,5 +1,17 @@
 package com.run.dao;
 
-public interface BookDao {
+import java.util.List;
 
+import com.run.entity.Book;
+import com.run.entity.Comment;
+
+public interface BookDao {
+	Book askbookinfo(int bid);
+	List<Book> askbooklist();
+	List<Book> searchbytitle(String bookname);
+	List<Comment> askcomment(int bid); 
+	
+	void insertBook(Book book);
+	void deleteBook(int bid);
+	
 }
