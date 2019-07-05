101cff5d1b2522ce25da546614ad5ee00b739a33
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-05 16:39:39 +0800

    Update insertuser.sql

--- code/sql/insertuser.sql
+++ code/sql/insertuser.sql
@@ -1,4 +1,4 @@
-/*insert into user values(0,"sf1","sf",0,0,0,0,"1653395382@qq.com",1,0);
-insert into user values(1,"sf","sf",0,0,0,0,"1653395382@qq.com",1,0);
-insert into user values(2,"sf3","sf",0,0,0,0,"1653395382@qq.com",0,0);*/
+/*insert into user values(0,"sf1","sf","user0",0,0,0,0,"1653395382@qq.com",1,0,0);
+insert into user values(1,"sf","sf","user1",0,0,0,0,"1207530580@qq.com",1,0,1);
+insert into user values(2,"sf3","sf","user2",0,0,0,0,"1456199702@qq.com",0,0,0);*/
 select * from user
