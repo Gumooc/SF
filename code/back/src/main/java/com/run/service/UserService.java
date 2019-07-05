fa42cbc05af893630d702200f8c70f74c21b32a3
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-05 10:11:49 +0800

    Update UserService.java

--- code/back/src/main/java/com/run/service/UserService.java
+++ code/back/src/main/java/com/run/service/UserService.java
@@ -1,6 +1,9 @@
 package com.run.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.run.entity.User;
+import com.run.entity.UserbookItem;
 
 import net.sf.json.JSONObject;
 
@@ -8,8 +11,14 @@ public interface UserService {
 	JSONObject logincheck(User user);
 	JSONObject register(User user);
 	JSONObject useractive(int uid);
+	
+	
 	JSONObject askcollector(int uid);
 	JSONObject askhistory(int uid);
+	
+	
 	JSONObject askcomment(int uid);
 	JSONObject askworks(int uid);
+	JSONObject askuser(int uid);
+	JSONObject setImg(int uid, MultipartFile file);
 }
