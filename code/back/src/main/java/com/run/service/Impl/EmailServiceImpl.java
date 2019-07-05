72b32ed1e9083deb7c286fbfb9af4984a02ee729
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-05 16:54:17 +0800

    Update EmailServiceImpl.java

--- code/back/src/main/java/com/run/service/Impl/EmailServiceImpl.java
+++ code/back/src/main/java/com/run/service/Impl/EmailServiceImpl.java
@@ -58,7 +58,7 @@ public class EmailServiceImpl implements EmailService {
             message.setSubject("账号激活");
             // 2.4设置邮件内容
             String content = "<html><head></head><body><h1>这是一封激活邮件,激活请点击以下链接</h1><h3><a href='http://localhost:8080/sfbook/user/activation?uid="
-                    +user.getUid()+"&active="+"1" + "'>http://localhost:8080/sfbook/user/activation?uid=" +user.getUid() +"&active="+"1"
+                    +user.getUid()+"&active="+"1" + "'>http://49.234.77.32:8080/sfbook(2)/user/activation?uid=" +user.getUid() +"&active="+"1"
                     + "</href></h3></body></html>";
             message.setContent(content, "text/html;charset=UTF-8");
             // 3.发送邮件
