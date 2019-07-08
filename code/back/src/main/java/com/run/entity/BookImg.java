f99b8b3c6368009242eec4149c25f3cfde189ae2
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-08 14:22:23 +0800

    Create BookImg.java

---
+++ code/back/src/main/java/com/run/entity/BookImg.java
@@ -0,0 +1,22 @@
+package com.run.entity;
+
+public class BookImg {
+	private int id;
+	private String img;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	public String getImg() {
+		return img;
+	}
+}
