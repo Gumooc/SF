40f531cde004429859781b19463ca478b8333536
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-08 14:22:27 +0800

    Update BookServiceImpl.java

--- code/back/src/main/java/com/run/service/Impl/BookServiceImpl.java
+++ code/back/src/main/java/com/run/service/Impl/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.run.service.Impl;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.alibaba.druid.util.Base64;
 import com.run.dao.BookDao;
 import com.run.entity.Book;
 import com.run.entity.BookAudio;
+import com.run.entity.BookImg;
 import com.run.entity.Comment;
 import com.run.service.BookService;
 
@@ -26,18 +29,37 @@ public class BookServiceImpl implements BookService {
 	
 	@Override
 	public Book askbookinfo(int bid) {
-		return bookMapper.askbookinfo(bid);
+		Book book = bookMapper.askbookinfo(bid);
+		Query query = new Query(Criteria.where("id").is(bid));
+		BookImg result=mongoTemplate.findOne(query, BookImg.class, "bookimg");
+		if (result != null) {
+			book.setImg(result.getImg());
+		}
+		return book;
 	}
 	
 	@Override
 	public List<Book> askbooklist(){
-		return bookMapper.askbooklist();
+		List<Book> booklist = bookMapper.askbooklist();
+		for (Book book:booklist) {
+			Query query = new Query(Criteria.where("id").is(book.getBid()));
+			BookImg result=mongoTemplate.findOne(query, BookImg.class, "bookimg");
+			if (result != null) {
+				book.setImg(result.getImg());
+			}
+		}
+		return booklist;
 	}
 	
 	@Override
 	public List<Book> searchbytitle(String bookname){
-		System.out.println("service:"+bookname);
-		return bookMapper.searchbytitle(bookname);
+		List<Book> booklist = bookMapper.askbooklist();
+		for (Book book:booklist) {
+			Query query = new Query(Criteria.where("id").is(book.getBid()));
+			BookImg result=mongoTemplate.findOne(query, BookImg.class, "bookimg");
+			book.setImg(result.getImg());
+		}
+		return booklist;
 	}
 	
 	@Override
@@ -48,6 +70,8 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public JSONObject insertBook(Book book) {
 		JSONObject feedback = new JSONObject();
+		int bid = bookMapper.getmaxid()+1;
+		book.setBid(bid);
 		bookMapper.insertBook(book);
 		feedback.put("resp", "s");
 		return feedback;
@@ -60,6 +84,37 @@ public class BookServiceImpl implements BookService {
 		feedback.put("resp", "s");
 		return feedback;
 	}
+
+	@Override
+	public JSONObject updateBook(Book book) {
+		JSONObject feedback = new JSONObject();
+		bookMapper.updateBook(book);
+		feedback.put("resp", "s");
+		return feedback;
+	}
+	
+
+	@Override
+	public JSONObject setBookimg(int bid, MultipartFile img) {
+		JSONObject feedback = new JSONObject();
+		String data = "";
+		try {
+			data = Base64.byteArrayToBase64(img.getBytes());
+			BookImg bookImg = new BookImg();
+			bookImg.setId(bid);
+			bookImg.setImg(data);
+			mongoTemplate.save(bookImg,"bookimg");
+			feedback.put("resp", "s");
+			feedback.put("body", "");
+		} catch (IOException e) {
+
+			feedback.put("resp", "f");
+			feedback.put("body", "");
+			e.printStackTrace();
+		}
+		return feedback;
+	}
+	
 	
 	@Override
 	public JSONObject askaudio(int bid, int index) {
@@ -93,12 +148,8 @@ public class BookServiceImpl implements BookService {
 	public JSONObject delaudio(int bid, int index) {
 		JSONObject feedback = new JSONObject();
 		Query query = new Query(Criteria.where("id").is(bid).and("index").is(index));
-		if (query != null) {
-			mongoTemplate.remove(query, BookAudio.class);
-			feedback.put("resp", "s");
-		} else {
-			feedback.put("resp", "f");
-		}
+		mongoTemplate.remove(query, BookAudio.class);
+		feedback.put("resp", "s");
 		return feedback;
 	}
 }
