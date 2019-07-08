2c8ecf31605fa1ad5351f24bde390fbea5f4c918
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-08 09:29:24 +0800

    Update BookAudio.java

--- code/back/src/main/java/com/run/entity/BookAudio.java
+++ code/back/src/main/java/com/run/entity/BookAudio.java
@@ -1,8 +1,19 @@
 package com.run.entity;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class BookAudio {
 	private int id;
-	private String audio;
+	private int index;
+	private MultipartFile audio;
+	
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
 	
 	public void setId(int id) {
 		this.id = id;
@@ -12,11 +23,11 @@ public class BookAudio {
 		return id;
 	}
 	
-	public void setAudio(String audio) {
+	public void setAudio(MultipartFile audio) {
 		this.audio = audio;
 	}
 	
-	public String getAudio() {
+	public MultipartFile getAudio() {
 		return audio;
 	}
 }
