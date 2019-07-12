b1fde50ca7c406292b48d7d5058ee475f1a388f7
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-12 17:07:48 +0800

    Update test.java

--- code/back/src/main/java/com/run/control/test.java
+++ code/back/src/main/java/com/run/control/test.java
@@ -1,20 +1,16 @@
 package com.run.control;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.type.TimeZoneType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.alibaba.druid.util.Base64;
 
 @Controller
 @RequestMapping("/test")
@@ -42,9 +38,9 @@ public class test {
 	@ResponseBody
 	@RequestMapping("/echot")
 	public String echot(@RequestParam(value="bid") int bid, @RequestParam(value="file", required = false) MultipartFile txt,HttpServletRequest request, HttpServletResponse response) {
-		setRHeader(request, response);
+		//setRHeader(request, response);
 		String stt = "ÄãºÃÂð";
-
+		
 		String data ="";
 		try {
 			data = new String(txt.getBytes());
@@ -52,7 +48,7 @@ public class test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(data);
+		System.out.println(stt);
 		return stt;
 	}
 }
