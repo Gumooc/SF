c6031815a78084def86cebab9e521f0a76604e63
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-24 08:46:16 +0800

    Update HistoryServiceImpl.java

--- code/back/src/main/java/com/run/service/Impl/HistoryServiceImpl.java
+++ code/back/src/main/java/com/run/service/Impl/HistoryServiceImpl.java
@@ -1,9 +1,12 @@
 package com.run.service.Impl;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.run.dao.BookDao;
 import com.run.dao.HistoryDao;
+import com.run.entity.Book;
 import com.run.entity.UserbookItem;
 import com.run.service.HistoryService;
 
@@ -13,6 +16,8 @@ import net.sf.json.JSONObject;
 public class HistoryServiceImpl implements HistoryService {
 	@Autowired
 	private HistoryDao historyMapper;
+	@Autowired
+	private BookDao bookMapper;
 	
 	@Override
 	public JSONObject clear(int uid) {
@@ -27,7 +32,9 @@ public class HistoryServiceImpl implements HistoryService {
 	@Override
 	public JSONObject insert(UserbookItem ubi) {
 		JSONObject feedback = new JSONObject();
-		historyMapper.insert(ubi);
+		Book book = bookMapper.askbookinfo(ubi.getBid());
+		//System.out.println(ubi.getUid()+";"+ ubi.getBid()+";"+ ubi.getLst()+";"+ book.getKind());
+		historyMapper.insert (ubi.getUid(), ubi.getBid(), book.getKind(), ubi.getLst());
 		feedback.put("resp", "s");
 		feedback.put("body", "");
 		return feedback;
