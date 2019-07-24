af0bd7763f81183e9e74e3d941c3931656a6c8dc
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-24 08:45:52 +0800

    Update HistoryDao.java

--- code/back/src/main/java/com/run/dao/HistoryDao.java
+++ code/back/src/main/java/com/run/dao/HistoryDao.java
@@ -1,8 +1,10 @@
 package com.run.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.run.entity.UserbookItem;
 
 public interface HistoryDao {
-	void insert(UserbookItem ubi);
+	void insert(@Param("uid") int uid, @Param("bid") int bid, @Param("kind") String kind, @Param("lst") String lst);
 	void clear(int uid);
 }
