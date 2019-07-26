51781112f7de5d4803b7612a4f283ce88d4eabde
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-26 10:49:33 +0800

    Update insertbook.sql

--- code/sql/insertbook.sql
+++ code/sql/insertbook.sql
@@ -3,3 +3,5 @@ insert into book values(2,0,"book2by0","0",0,1,3);
 insert into book values(3,0,"book3by0","0",0,1,10);*/
 #update book set lst="2019-07-03";
 select * from book;
+#select * from book order by lst desc limit 5;
+
