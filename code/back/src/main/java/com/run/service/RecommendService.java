f1b0e59668d27b8851b800994fe69ed417aed4e3
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-29 08:30:03 +0800

    Update RecommendService.java

--- code/back/src/main/java/com/run/service/RecommendService.java
+++ code/back/src/main/java/com/run/service/RecommendService.java
@@ -1,13 +1,9 @@
 package com.run.service;
 
-import java.io.IOException;
-import java.util.List;
 
-import org.apache.mahout.cf.taste.common.TasteException;
-
-import com.run.entity.Book;
+import net.sf.json.JSONObject;
 
 public interface RecommendService {
 
-	List<Book> recommend(int uid) throws TasteException, IOException;
+	JSONObject recommend(int uid);
 }
