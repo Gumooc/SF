aa55755af4a7d1c3abf1104f47534b61d3fa0b82
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-29 17:11:30 +0800

    Create TpUser.java

---
+++ code/back/src/main/java/com/run/entity/TpUser.java
@@ -0,0 +1,22 @@
+package com.run.entity;
+
+public class TpUser {
+	private int uid;
+	private String openid;
+	
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+	public int getUid() {
+		return uid;
+	}
+	
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+	
+	public String getOpenid() {
+		return openid;
+	}
+}
