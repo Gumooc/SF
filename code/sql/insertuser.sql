b1f9ac1133179929cc2a76212d8f1758b2189e1d
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-29 08:26:46 +0800

    Update insertuser.sql

--- code/sql/insertuser.sql
+++ code/sql/insertuser.sql
@@ -1,4 +1,5 @@
 /*insert into user values(0,"sf1","sf","user0",0,0,0,0,"1653395382@qq.com",1,0,0);
 insert into user values(1,"sf","sf","user1",0,0,0,0,"1207530580@qq.com",1,0,1);
 insert into user values(2,"sf3","sf","user2",0,0,0,0,"1456199702@qq.com",0,0,0);*/
+#alter table user add birth date default "0000-01-01";
 select * from user
