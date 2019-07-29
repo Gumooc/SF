41a45dc5c9f4001fbeec312b9e44c3d335a5d08e
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-07-29 08:29:58 +0800

    Update RecommendServiceImpl.java

--- code/back/src/main/java/com/run/service/Impl/RecommendServiceImpl.java
+++ code/back/src/main/java/com/run/service/Impl/RecommendServiceImpl.java
@@ -1,6 +1,7 @@
 package com.run.service.Impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -14,49 +15,94 @@ import org.apache.mahout.cf.taste.model.JDBCDataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.run.dao.BookDao;
 import com.run.entity.Book;
+import com.run.entity.BookImg;
 import com.run.service.RecommendService;
+
+import net.sf.json.JSONObject;
 
 @Service
 public class RecommendServiceImpl implements RecommendService {
+	@Autowired
+	private BookDao bookMapper;
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
 	@Override
-	public List<Book> recommend(int uid) throws TasteException, IOException {
-		Properties prop = PropertiesLoaderUtils.loadProperties(new ClassPathResource("/resources/jdbc.properties"));
-		System.out.println(new ClassPathResource("../resources/jdbc.properties"));
-		String userName = prop.getProperty("jdbc.userName");
-		String password = prop.getProperty("jdbc.password");
+	public JSONObject recommend(int uid){
+		JSONObject feedbody = new JSONObject();
+		List<Book> recommendlist = new ArrayList<>();
+		try {
+			recommendlist = getrecommend(uid);
+		} catch (TasteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<Book> latestlist = getlatest();
+		List<Book> hottestlist = gethottest();
+		feedbody.put("recommend", recommendlist);
+		feedbody.put("latest", latestlist);
+		return feedbody;
+	}
+	
+	private List<Book> getrecommend(int uid) throws TasteException{
+
+		//Properties prop = PropertiesLoaderUtils.loadProperties(new ClassPathResource("/resources/jdbc.properties"));
+		//System.out.println(new ClassPathResource("../resources/jdbc.properties"));
+		//String userName = prop.getProperty("jdbc.userName");
+		//String password = prop.getProperty("jdbc.password");
 		MysqlDataSource dataSource = new MysqlDataSource();
+		String userName = "root";
+		String password = "234567";
 		dataSource.setServerName("localhost");
 		dataSource.setPort(3306);
 		dataSource.setDatabaseName("sfbook");
 		dataSource.setUser(userName);
 		dataSource.setPassword(password);
 		
-		 String file = "G:/test.csv";
-	        //DataModel model = new FileDataModel(new File(file));
-		 	JDBCDataModel model = new MySQLJDBCDataModel(dataSource, "history", "uid", "kind", "prc", "date");
-	        UserSimilarity user = new EuclideanDistanceSimilarity(model);
-	        NearestNUserNeighborhood neighbor = new NearestNUserNeighborhood(2, user, model);
-	        //Recommender r = new GenericUserBasedRecommender(model, neighbor, user);
-	        Recommender r = new GenericBooleanPrefUserBasedRecommender(model, neighbor, user);
-	        LongPrimitiveIterator iter = model.getUserIDs();
-
-	        while (iter.hasNext()) {
-	            long uuid = iter.nextLong();
-	            List<RecommendedItem> list = r.recommend(uuid, 3);
-	            System.out.printf("uid:%s", uuid);
-	            for (RecommendedItem ritem : list) {
-	                System.out.printf("(%s,%f)", ritem.getItemID(), ritem.getValue());
-	            }
-	            System.out.println();
-	        }
-		return null;
+		//String file = "G:/test.csv";
+	    //DataModel model = new FileDataModel(new File(file));
+		JDBCDataModel model = new MySQLJDBCDataModel(dataSource, "history", "uid", "kind", "prc", "date");
+	    UserSimilarity user = new EuclideanDistanceSimilarity(model);
+	    NearestNUserNeighborhood neighbor = new NearestNUserNeighborhood(2, user, model);
+	    //Recommender r = new GenericUserBasedRecommender(model, neighbor, user);
+	    Recommender r = new GenericBooleanPrefUserBasedRecommender(model, neighbor, user);
+	    
+	    List<Book> booklist = new ArrayList<Book>();
+	    
+	    List<RecommendedItem> list = r.recommend(uid, 3);
+		for (RecommendedItem ritem : list) {
+	    	int bid = (int) ritem.getItemID();
+	    	Book book = bookMapper.askbookinfo(bid);
+	    	if (book == null) continue;
+			Query query = new Query(Criteria.where("id").is(bid));
+			BookImg result=mongoTemplate.findOne(query, BookImg.class, "bookimg");
+			if (result != null) {
+				book.setImg(result.getImg());
+			}
+			book.setNickname(bookMapper.getauthor(book.getUid()));
+			booklist.add(book);
+	    }
+		    
+		return booklist;
 	}
 	
+	private List<Book> getlatest() {
+		List<Book> booklist = bookMapper.asklatest(3);
+		return booklist;
+	}
+	
+	private List<Book> gethottest() {
+		List<Book> booklist;
+		return null; 
+	}
 
 }
