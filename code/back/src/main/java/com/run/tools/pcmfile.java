9d3050b9660d0b6e7619481855da67275106d336
slzxcdw <45347405+slzxcdw@users.noreply.github.com>
2019-07-30 10:18:15 +0800

    Add files via upload

---
+++ code/back/src/main/java/com/run/tools/pcmfile.java
@@ -0,0 +1,20 @@
+package com.run.tools;
+
+import java.util.Comparator;
+
+public class pcmfile {
+    private String name;
+    private Double starttime;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getStarttime() {
+        return starttime;
+    }
+    public void setStarttime(Double starttime) {
+        this.starttime = starttime;
+    }
+}
