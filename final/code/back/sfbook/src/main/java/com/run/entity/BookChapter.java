c918d6cfc2304dc2867957769778bcf3c7d730c5
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-07 11:40:29 +0800

    Create BookChapter.java

---
+++ final/code/back/sfbook/src/main/java/com/run/entity/BookChapter.java
@@ -0,0 +1,22 @@
+package com.run.entity;
+
+public class BookChapter {
+	private int id;
+	private String chapter;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setChapter(String chapter) {
+		this.chapter = chapter;
+	}
+	
+	public String getChapter() {
+		return chapter;
+	}
+}
