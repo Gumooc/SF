7f0d026dcb0db4d2b1267203e6d8ec30b2519e1f
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-07 11:41:16 +0800

    Create CommentServiceImpl.java

---
+++ final/code/back/sfbook/src/main/java/com/run/service/Impl/CommentServiceImpl.java
@@ -0,0 +1,50 @@
+package com.run.service.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import com.run.dao.CommentDao;
+import com.run.entity.Comment;
+import com.run.entity.CommentDes;
+import com.run.service.CommentService;
+
+import net.sf.json.JSONObject;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+	@Autowired
+	private CommentDao commentMapper;
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	@Override
+	public JSONObject insert(Comment comment) {
+		JSONObject feedback = new JSONObject();
+		feedback.put("resp", "f");
+		commentMapper.insert(comment);
+		CommentDes commentDes = new CommentDes();
+		comment.setCid(comment.getCid());
+		commentDes.setId(comment.getCid());
+		commentDes.setDes(comment.getDes());
+		mongoTemplate.save(commentDes,"comment");
+		feedback.put("resp", "s");
+		return feedback;
+	}
+
+	@Override
+	public JSONObject delete(Comment comment) {
+		JSONObject feedback = new JSONObject();
+		feedback.put("resp", "f");
+		
+		Query query = new Query(Criteria.where("_id").is(comment.getCid()));
+		mongoTemplate.remove(query, "comment");
+		commentMapper.delete(comment);
+
+		feedback.put("resp", "s");
+		return feedback;
+	}
+
+}
