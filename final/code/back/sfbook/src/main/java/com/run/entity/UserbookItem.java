47bae613ea924eaa5aea9442651fd57196025505
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-07 11:40:51 +0800

    Create UserbookItem.java

---
+++ final/code/back/sfbook/src/main/java/com/run/entity/UserbookItem.java
@@ -0,0 +1,31 @@
+package com.run.entity;
+
+public class UserbookItem {
+	private int uid;
+	private int bid;
+	private String lst;
+	
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	
+	public int getUid() {
+		return uid;
+	}
+	
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	
+	public int getBid() {
+		return bid;
+	}
+	
+	public void setLst(String lst) {
+		this.lst = lst;
+	}
+	
+	public String getLst() {
+		return lst;
+	}
+}
