fd00fe7b2674e9c4f59c057e9644869ea7d6a689
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-07 11:40:27 +0800

    Create BookAudio.java

---
+++ final/code/back/sfbook/src/main/java/com/run/entity/BookAudio.java
@@ -0,0 +1,31 @@
+package com.run.entity;
+
+public class BookAudio {
+	private int id;
+	private int index;
+	private String audio;
+	
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+	
+	public String getAudio() {
+		return audio;
+	}
+}
