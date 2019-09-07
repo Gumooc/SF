946497cd1d16e24de365194bd59de5fceb3fd6e0
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-07 11:41:34 +0800

    Create UserService.java

---
+++ final/code/back/sfbook/src/main/java/com/run/service/UserService.java
@@ -0,0 +1,30 @@
+package com.run.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.run.entity.User;
+import net.sf.json.JSONObject;
+
+public interface UserService {
+	JSONObject logincheck(User user);
+	JSONObject register(User user);
+	JSONObject useractive(int uid);
+	
+	JSONObject updateuser(User user);
+	JSONObject askcollector(int uid);
+	JSONObject askhistory(int uid);
+	
+	JSONObject mdfypassword(int uid, String oldp, String newp);
+	
+	JSONObject askcomment(int uid);
+	JSONObject askworks(int uid);
+	JSONObject askselfworks(int uid);
+	JSONObject askuser(int uid);
+	JSONObject setImg(int uid, MultipartFile file);
+	
+	int tplogin(JSONObject info);
+	
+	JSONObject updatepassword(int uid, String password);
+	User modifycheck(String username, String email);
+	
+}
