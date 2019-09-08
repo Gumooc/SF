7e2069cbdc55ba935ec0157f7a68dd17c3efa0c7
Gumooc <52364819+Gumooc@users.noreply.github.com>
2019-09-08 11:30:07 +0800

    Update CreateBookService.java

--- code/back/src/main/java/com/run/service/CreateBookService.java
+++ code/back/src/main/java/com/run/service/CreateBookService.java
@@ -1,14 +1,13 @@
 package com.run.service;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.multipart.MultipartFile;
 
 import net.sf.json.JSONObject;
 
+import javax.servlet.http.HttpSession;
+
 public interface CreateBookService {
 
 	JSONObject bytext(HttpSession session, JSONObject info, MultipartFile txt) throws Exception;
-	JSONObject bysound(HttpSession session, JSONObject info, MultipartFile sound) throws Exception;
-	
+	JSONObject bysound(HttpSession session,JSONObject info, MultipartFile sound) throws Exception;
 }
